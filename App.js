1a3f3a9f792c5c4319c3dd4344d744b7f7783180
Chris Bull <chris@chrisbull.com>
2017-12-10 03:48:28 -0800

    FINISH CARD

--- App.js
+++ App.js
@@ -18,14 +18,14 @@ import SwipeCard from './SwipeCard'
 
 const { height: WINDOW_HEIGHT, width: WINDOW_WIDTH } = Dimensions.get('window')
 
-const SCREEN_HEIGHT = WINDOW_HEIGHT // or passed variable
-const SCREEN_WIDTH = WINDOW_WIDTH // or passed variable
-const GAP = 20
-const PEEK = 30
-// const SLIDE_PADDING = GAP / 2
-const SLIDE_WIDTH = SCREEN_WIDTH - 2 * PEEK
-const CARD_WIDTH = SLIDE_WIDTH - GAP
-const CARD_HEIGHT = SCREEN_HEIGHT * 0.8
+let SCREEN_HEIGHT = WINDOW_HEIGHT // or passed variable
+let SCREEN_WIDTH = WINDOW_WIDTH // or passed variable
+let GAP = 20
+let PEEK = 30
+// let SLIDE_PADDING = GAP / 2
+let SLIDE_WIDTH = SCREEN_WIDTH - 2 * PEEK
+let CARD_WIDTH = SLIDE_WIDTH - GAP
+let CARD_HEIGHT = SCREEN_HEIGHT * 0.8
 
 class CardItem extends React.PureComponent {
   render() {
@@ -42,7 +42,7 @@ class CardItem extends React.PureComponent {
 
 export default class App extends Component<{}> {
   _renderCard = props => <CardItem {...props} />
-  _renderSwipeCard = props => <SwipeCard stack={false} cards={[{}]} renderCard={this._renderCard} />
+  _renderSwipeCard = props => <SwipeCard cards={[{}]} renderCard={this._renderCard} />
 
   // _renderSwiper = () => (
   //   <Swiper containerStyle={styles.swiperContainer} style={styles.swiper} loop={false} index={0}>
@@ -64,29 +64,29 @@ export default class App extends Component<{}> {
   //   </Swiper>
   // )
 
-  _renderCarousel = () => (
-    <View style={styles.container}>
-      <Carousel
-        ref={c => (this._carousel = c)}
-        data={[{}, {}, {}, {}]}
-        renderItem={this._renderItem}
-        sliderWidth={SCREEN_WIDTH}
-        itemWidth={CARD_WIDTH}
-        // hasParallaxImages={true}
-        // firstItem={1}
-        // inactiveSlideScale={0.8}
-        inactiveSlideOpacity={0.7}
-        enableMomentum //={false}
-        containerCustomStyle={styles.swiper}
-        contentContainerCustomStyle={styles.swiperContent}
-        loop={false}
-        // autoplay={true}
-        // autoplayDelay={500}
-        // autoplayInterval={3000}
-        onSnapToItem={index => this.setState({ slider1ActiveSlide: index })}
-      />
-    </View>
-  )
+  // _renderCarousel = () => (
+  //   <View style={styles.container}>
+  //     <Carousel
+  //       ref={c => (this._carousel = c)}
+  //       data={[{}, {}, {}, {}]}
+  //       renderItem={this._renderItem}
+  //       sliderWidth={SCREEN_WIDTH}
+  //       itemWidth={CARD_WIDTH}
+  //       // hasParallaxImages={true}
+  //       // firstItem={1}
+  //       // inactiveSlideScale={0.8}
+  //       inactiveSlideOpacity={0.7}
+  //       enableMomentum //={false}
+  //       containerCustomStyle={styles.swiper}
+  //       contentContainerCustomStyle={styles.swiperContent}
+  //       loop={false}
+  //       // autoplay={true}
+  //       // autoplayDelay={500}
+  //       // autoplayInterval={3000}
+  //       onSnapToItem={index => this.setState({ slider1ActiveSlide: index })}
+  //     />
+  //   </View>
+  // )
 
   render() {
     return this._renderSwipeCard()
